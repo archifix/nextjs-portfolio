0db73550ae368c570d7da53f324ed85f69bcecb5
Илья <107472693+archifix@users.noreply.github.com>
2023-01-14 14:28:05 +0300

    Update

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,65 +1,110 @@
-import { Link } from 'react-scroll'
-import Logo from '../Logo/Logo'
+import { Link } from "react-scroll"
+import { useMediaQuery } from "@/hooks"
+import { useState } from "react"
+import Logo from "../Logo/Logo"
 
-import styles from '../../styles/header.module.scss'
+import styles from "../../styles/header.module.scss"
+import stylesMenu from "../../styles/mobileMenu.module.scss"
 
 const Header = () => {
+  const isMobile = useMediaQuery(640)
+  const [menuOpen, setMenuOpen] = useState(false)
   const spy = true
   const smooth = true
   const offset = 140
   const duration = 500
+  const currentMenuItemClass = isMobile
+    ? stylesMenu.menu__item
+    : styles.header__nav__list__item
+
+  const handleToggleMenu = () => {
+    ;(document.querySelector("body") as HTMLBodyElement).classList.toggle(
+      "overflow-hidden"
+    )
+    setMenuOpen(!menuOpen)
+  }
+
+  const closeMenu = () => {
+    ;(document.querySelector("body") as HTMLBodyElement).classList.toggle(
+      "overflow-hidden"
+    )
+    setMenuOpen(false)
+  }
+
   return (
-    <header>
-      <div className={`container ${styles.header_container}`}>
+    <header className={styles.header}>
+      <div className={`container ${styles.header__container}`}>
         <Logo />
-        <nav className={styles.header_nav}>
-          <ul className={styles.header_nav_list}>
-            <li className={styles.header_nav_list_item}>
+        {isMobile && (
+          <button
+            onClick={handleToggleMenu}
+            className={`${styles.burger_menu} ${menuOpen ? styles.open : ""}`}
+          >
+            <span />
+            <span />
+            <span />
+          </button>
+        )}
+        <nav
+          className={`${isMobile ? stylesMenu.menu : styles.header__nav} ${
+            menuOpen ? stylesMenu.open : ""
+          }`}
+        >
+          <ul
+            className={`${
+              isMobile ? styles.list_reset : styles.header__nav__list
+            }`}
+          >
+            <li className={styles.header__nav__list__item}>
               <Link
-                href='/'
-                to='about'
+                href="/"
+                to="about"
                 spy={spy}
                 smooth={smooth}
                 offset={offset}
                 duration={duration}
-                >
-                  Обо мне
+                className={styles.header__nav__list__item__link}
+              >
+                Обо мне
               </Link>
             </li>
-            <li className={styles.header_nav_list_item}>
+            <li className={styles.header__nav__list__item}>
               <Link
-                href='/'
-                to='about'
+                href="/"
+                to="about"
                 spy={spy}
                 smooth={smooth}
                 offset={offset}
                 duration={duration}
-                >
-                  Навыки
+                className={styles.header__nav__list__item__link}
+              >
+                Навыки
               </Link>
             </li>
-            <li className={styles.header_nav_list_item}>
+            <li className={styles.header__nav__list__item}>
               <Link
-                href='/'
-                to='about'
+                href="/"
+                to="about"
                 spy={spy}
                 smooth={smooth}
                 offset={offset}
                 duration={duration}
-                >
-                  Портфолио
+                className={styles.header__nav__list__item__link}
+              >
+                Портфолио
               </Link>
             </li>
-            <li className={styles.header_nav_list_item}>
+            <li className={styles.header__nav__list__item}>
               <Link
-                href='/'
-                to='about'
+                href="/"
+                to="about"
                 spy={spy}
                 smooth={smooth}
                 offset={offset}
                 duration={duration}
-                >
-                  Связаться со мной
+                className={styles.header__nav__list__item__link}
+              >
+                Связаться со мной
               </Link>
             </li>
           </ul>
